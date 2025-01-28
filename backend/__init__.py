fc44850cdf9b170a91f01b4cfa1cafe58ed5a264
Marcos Crispim <contato.marcoscrispim@gmail.com>
2025-01-27 23:31:00 -0300

    Atualizando crud e design

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,27 @@
+import os
 from flask import Flask
-
-app = Flask()
-
-@app.route('/')
-def hello():
-    return 'Hello mundo'
+from . import db, auth, blog
+# para executar o código, digite no terminal: flask --app nome_do_arquivo run --debug
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    db.init_app(app)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+# http://127.0.0.1:5000/hello essa vai ser a rota que foi definida aqui
+    
+    return app
